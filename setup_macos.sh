61f38dc4bafffd519c74da9f5738ceface40870c
Egor Kotelnikov <eakotelnikov@ya.ru>
2020-11-09 08:43:05 +0300

    Create setup_macos.sh

---
+++ setup_macos.sh
@@ -0,0 +1,15 @@
+#install firefox
+#setup userChrome.css file
+
+#install djvu2pdf + ocrmypdf + languages pack
+#ocr files custom script
+
+#install jetbrain products
+
+# acrobat reader
+# vlc
+#telegram, whatsapp
+#yandex disk
+
+
+
