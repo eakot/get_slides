410d73193f5844e7fec5bd2ea9e645cf5a176120
kotelnikov <eakotelnikov@ya.ru>
2021-10-03 23:37:13 +0300

    major improvements

--- src/server.py
+++ src/server.py
@@ -1,19 +1,34 @@
-from flask import Flask
+import pytube
+from pytube import YouTube
+from flask import Flask, url_for, redirect
 from flask import render_template, send_file, request
 import pandas as pd
 from sqlalchemy import create_engine
 from flask_sqlalchemy import SQLAlchemy
-from pytube import YouTube
 from datetime import datetime
-import cv2
-import os
+
+from .get_slides import detect_unique_screenshots, initialize_output_folder
 from .utils import *
+from loguru import logger
+import logging
+from sys import stdout
+import json
+
+logger.add(stdout, colorize=True, format="<green>{time}</green> <level>{message}</level>")
+
+
+# create a custom handler
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        logger_opt = logger.opt(depth=6, exception=record.exc_info)
+        logger_opt.log(record.levelno, record.getMessage())
+
 
 engine = create_engine('postgresql://postgres:postgres@db:5432/postgres')
 
 app = Flask(__name__, template_folder='../templates/', static_folder='../static')
 app.config.from_object("src.config.Config")
-
+app.logger.addHandler(InterceptHandler())
 db = SQLAlchemy(app)
 
 video_directory = "static/videos/"
@@ -22,23 +37,68 @@ video_directory = "static/videos/"
 class Video(db.Model):
     __tablename__ = "video"
 
-    id = db.Column(db.Integer, primary_key=True)
+    video_id = db.Column(db.String, primary_key=True)
     url = db.Column(db.String, nullable=False)
-    # filename = db.Column(String, unique=True, nullable=False)
-    title = db.Column(db.String)
+    filename = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    caption_tracks = db.Column(db.String, nullable=False)
+    captions_xml_aen = db.Column(db.Text, nullable=False)
+    channel_id = db.Column(db.String, nullable=False)
+    channel_url = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    keywords = db.Column(db.String, nullable=False)
+    length_sec = db.Column(db.Integer, nullable=False)
+    publish_date = db.Column(db.Date, nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    download_datetime = db.Column(db.Date, nullable=False)
+    slides_with_text = db.Column(db.Text)
 
-    def __init__(self, engine, url, video_directory):
+    def __init__(self, video_id):
         self.directory = video_directory
-
-        self.url = url
-        self.filename = generate_name_from_url(url)
-
+        self.download_datetime = datetime.now()
+        self.url = f"https://youtu.be/{video_id}"
+        logger.info(f"url = {self.url}")
+        self.video_id = video_id
+        logger.info(f"video_id = {self.video_id}")
+        self.filename = f"{self.video_id}.mp4"
+        logger.info(f"Downloading is being started... out = {self.filename}")
         self.youtube_object = YouTube(self.url)
 
-        self.youtube_info = self.get_video_info(self.youtube_object)
+        self.author = self.youtube_object.author
+
+        self.caption_tracks = str(self.youtube_object.caption_tracks)
+
+        self.captions_xml_aen = self.youtube_object.captions['a.en'].xml_captions
+
+        self.channel_id = self.youtube_object.channel_id
+        self.channel_url = self.youtube_object.channel_url
+        self.description = self.youtube_object.description
+        self.keywords = self.youtube_object.keywords
+        self.length_sec = self.youtube_object.length
+        self.publish_date = self.youtube_object.publish_date
+        self.title = self.youtube_object.title
+        self.views = self.youtube_object.views
+        self.download_datetime = datetime.now()
 
         self.download()
+        # self.frames_filenames = self.save_unique_frames()
         self.frames_filenames = self.save_frames()
+
+        frames_text_pairs = [[frame, ocr_image(frame)] for frame in self.frames_filenames]
+
+        frames_text_df = pd.DataFrame(frames_text_pairs, columns=["image", "text"])
+
+        # duplicates = remove_duplicates_from_disk(frames_text_df)
+
+        duplicates = remove_duplicates(frames_text_pairs)
+
+        slides = sorted(set(self.frames_filenames) - set(duplicates))
+        frames_text_dict = {i[0]: i[1] for i in frames_text_pairs}
+
+        slides_with_text_dict = {slide: frames_text_dict[slide] for slide in slides}
+
+        self.slides_with_text = json.dumps(slides_with_text_dict)
 
     def download(self):
         video = self.youtube_object.streams \
@@ -48,6 +108,14 @@ class Video(db.Model):
         video.download(output_path=self.directory, filename=self.filename)
 
         return None
+
+    def save_unique_frames(self):
+        video_path = os.path.join(self.directory, self.filename)
+        frames_dir = f"static/images/{self.video_id}"
+        initialize_output_folder(video_path, frames_dir)
+        frames_filenames = detect_unique_screenshots(video_path,
+                                                     frames_dir)
+        return frames_filenames
 
     def save_frames(self):
         images_dir = generate_dir(self.filename)
@@ -68,7 +136,7 @@ class Video(db.Model):
                 cv2.imwrite(image_filename, image)  # save frame as JPG file
             return hasFrames
 
-        sec = 0
+        sec = 5
         frame_rate = 10  # //it will capture image in each n second
         count = 1
         success = get_frame(sec)
@@ -80,54 +148,49 @@ class Video(db.Model):
 
         return frames_filenames
 
-    def get_video_info(self, youtube_object):
-        info = {
-            "author": youtube_object.author,
-            "caption_tracks": youtube_object.caption_tracks,
-            "captions": youtube_object.captions,
-            "channel_id": youtube_object.channel_id,
-            "channel_url": youtube_object.channel_url,
-            "description": youtube_object.description,
-            "keywords": youtube_object.keywords,
-            "length_sec": youtube_object.length,
-            "publish_date": youtube_object.publish_date,
-            "title": youtube_object.title,
-            "views": youtube_object.views,
-            "download_datetime": datetime.now()
-        }
 
-        return info
-
-
+@logger.catch
 @app.route('/get_slides/')
 def index():
+    logger.info("Index requested")
     return render_template('get_slides/index.html')
 
 
+@logger.catch
 @app.route('/get_slides/', methods=['POST', 'GET'])
 def index_upload():
     url = request.form['urlInput']
-    video = Video(engine, url, video_directory)
+    video_id = pytube.extract.video_id(url)
+    logger.info(f"Add video request, url = {url}")
 
-    db.session.add(video)
-    db.session.commit()
+    video_exists = Video.query.filter_by(video_id=video_id).first()
 
-    frames_text_pairs = [[frame, ocr_image(frame)] for frame in video.frames_filenames]
+    if video_exists:
+        logger.info(f"Requested video {video_id} already exist in database")
+        return redirect(url_for('show_slides', video_id=video_id))
 
-    frames_text_df = pd.DataFrame(frames_text_pairs, columns=["image", "text"])
+    else:
+        video = Video(video_id)
 
-    duplicates = remove_duplicates_from_disk(frames_text_df)
+        db.session.add(video)
+        db.session.commit()
 
-    slides = sorted(set(video.frames_filenames) - set(duplicates))
+        slides_with_text = json.loads(video.slides_with_text)
+        return render_template('get_slides/uploaded.html',
+                               slides_with_text=slides_with_text)
 
-    frames_text_dict = {i[0]: i[1] for i in frames_text_pairs}
-    slides_with_text = {slide: frames_text_dict[slide] for slide in slides}
-    return render_template('get_slides/uploaded.html',
-                           url=video.url,
-                           filename=video.filename,
-                           youtube_info=video.youtube_info,
-                           frames_text_dict=frames_text_dict,
-                           slides_with_text=slides_with_text)
+
+@logger.catch
+@app.route('/get_slides/show/<video_id>', methods=['POST', 'GET'])
+def show_slides(video_id):
+    video = Video.query.filter_by(video_id=video_id).first()
+
+    if video:
+        return render_template('get_slides/uploaded.html',
+                               slides_with_text=json.loads(video.slides_with_text))
+    else:
+        return redirect(url_for('index',
+                                error=f"Send video https://youtu.be/{video_id} to create slides"))
 
 
 @app.route("/")
--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,90 @@
-from pytube import YouTube
+import re
+
 import cv2
 import os
-import hashlib
-
+import numpy as np
 import pytesseract
-
+import fire
+from loguru import logger
 try:
     from PIL import Image
 except ImportError:
     import Image
 
+from difflib import SequenceMatcher
+
+
+def images_ocr_similar(a, b):
+    text_a = ocr_image(a)
+    text_b = ocr_image(b)
+
+    print(text_a)
+    print(text_b)
+    return similar(a, b)
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def remove_duplicates(frames_text_list):
+    duplicates = []
+    text_prev = frames_text_list[0][1]
+    for i in range(1, len(frames_text_list)):
+        image = frames_text_list[i][0]
+        text = frames_text_list[i][1]
+        sim = similar(text_prev, text)
+        logger.info(f"i = {i}, sim = {sim}")
+        if sim > 0.8:
+            duplicates.append(image)
+        else:
+            text_prev = text
+
+    logger.info(f"duplicates: {duplicates}")
+    for duplicated_frame in duplicates:
+        os.remove(duplicated_frame)
+
+    return duplicates
+
 
 def remove_duplicates_from_disk(frames_text_df):
     duplicates = frames_text_df["image"][frames_text_df.duplicated("text")] \
         .to_list()
+
     for duplicated_frame in duplicates:
         os.remove(duplicated_frame)
-        
+
     return duplicates
 
 
 def ocr_image(filename):
-    return pytesseract \
-        .image_to_string(Image.open(filename),
-                         lang='eng+rus')
+    image = cv2.imread(filename)
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    # image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    # image = cv2.Canny(image, 100, 200)
+
+    text = pytesseract \
+        .image_to_string(image, lang='eng')
+
+    text = "\n".join([ll.rstrip() for ll in text.splitlines() if ll.strip()])
+
+    text = re.compile(r"[^a-zA-Z0-9-\n]+").sub(" ", text)
+
+    return text
 
 
 def generate_name_from_url(url):
-    return hashlib.sha1(url.encode('utf-8')).hexdigest() + ".mp4"
+    return
 
 
 def generate_dir(filename):
     return f'static/images/{"_".join(filename.split(".")[:-1])}'
 
+
+if __name__ == '__main__':
+    fire.Fire()
